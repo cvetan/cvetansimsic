7d551a3ef367dae244b769ebb8e9940ef783331d
Cvetan Šimšić <cvetan.simsic@gmail.com>
2019-07-04 12:01:37 +0200

    Fix User database factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,13 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->name,
+        'last_name' => $faker->lastName,
+        'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
+        'is_admin' => $faker->randomElement([0, 1]),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => 'password', // password
         'remember_token' => Str::random(10),
     ];
 });
