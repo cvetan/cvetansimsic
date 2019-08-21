093502ccfa94618b41d4a998ff0b26d846b1f04a
Cvetan Šimšić <cvetan.simsic@gmail.com>
2019-08-21 11:26:49 +0200

    Configure Laracasts JavaScript. Remove double loop in pages index.

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -49,30 +49,6 @@
                         </td>
                     </tr>
                 @endforeach
-
-                @foreach ($pages as $page)
-                    <tr>
-                        <td>{{ $page->id }}</td>
-                        <td>{{ $page->name }}</td>
-                        <td>{{ $page->slug }}</td>
-                        <td>
-                            <a href="{{ route('admin.pages.show', ['page' => $page->id]) }}" class="btn btn-success" target="_blank">
-                                <i class="fa fa-eye m-r-2"></i>
-                                <span>{{ __('general.preview') }}</span>
-                            </a>
-
-                            <a href="{{ route('admin.pages.edit', ['page' => $page->id]) }}" class="btn btn-flat btn-warning">
-                                <i class="fa fa-pencil m-r-2"></i>
-                                <span>{{ __('general.edit') }}</span>
-                            </a>
-
-                            <a href="#" class="btn btn-danger">
-                                <i class="fa fa-minus-circle"></i>
-                                <span>{{ __('general.remove') }}</span>
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
             </tbody>
         </table>
     @endif
